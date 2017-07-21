15593eac2b0ba1fb01452300c071aa598a2077fc
Unknown <ober.robert24@gmail.com>
2017-07-21 10:36:07 +0200

    funk1

---
+++ PixelControl/PixelControl/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace PixelControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PixelControl.Properties.Resources._8_1;
+            this.pictureBox2.Location = new System.Drawing.Point(467, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 43);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PixelControl.Properties.Resources.rechner;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(263, 323);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(573, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(573, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(573, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(281, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 316);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(556, 71);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(269, 316);
+            this.listBox2.TabIndex = 11;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(381, 403);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(553, 398);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 451);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ PixelControl/PixelControl/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace PixelControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        public static Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = GetColorAt( 100, 100).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            b = new Bitmap(pictureBox1.Image);
+            b2 = new Bitmap(pictureBox2.Image);
+
+            label3.Text = "Breite: " + pictureBox1.Image.Width;
+            label2.Text = "Höhe: " + pictureBox1.Image.Height;
+            label6.Text = "Erster Pixel: " + b.GetPixel(1, 1);
+
+
+
+
+            label4.Text = "Breite: " + pictureBox2.Image.Width;
+            label5.Text = "Höhe: " + pictureBox2.Image.Height;
+            label7.Text = "Erster Pixel: " + b2.GetPixel(1, 1); ;
+            
+        }
+
+        string pixel;
+        string pixel2;
+        Bitmap b;
+        Bitmap b2;
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            for (int i = 1; i < pictureBox1.Image.Height; i++)
+            {
+                for (int j = 1; j < pictureBox1.Image.Width; j++)
+                {
+                    //listBox1.Items.Add(b.GetPixel(i, j));
+                    pixel = b.GetPixel(j, i).Name;
+
+                }
+
+            }
+
+            for (int i = 1; i < pictureBox2.Image.Height; i++)
+            {
+                for (int j = 1; j < pictureBox2.Image.Width; j++)
+                {
+                    //listBox1.Items.Add(b.GetPixel(i, j));
+                    pixel = b2.GetPixel(j, i).Name;
+
+                }
+
+            }
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            if (e.ProgressPercentage == 1)
+            {
+                listBox1.Items.Add(pixel);
+                label8.Text = listBox1.Items.Count.ToString();
+
+                listBox2.Items.Add(pixel);
+                label9.Text = listBox2.Items.Count.ToString();
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            findPicture picfind = new findPicture(pictureBox1.Image);
+            Point p = picfind.findPictureCoords(pictureBox2.Image);
+            
+            label9.Text = p.ToString();
+            //Graphics g = new Graphics();
+            Bitmap b = new Bitmap(pictureBox1.Image);
+            b.SetPixel(p.X, p.Y, Color.Black);
+            b.SetPixel(p.X+1, p.Y+1, Color.Black);
+            b.SetPixel(p.X + 2, p.Y + 2, Color.Black);
+            b.SetPixel(p.X + 3, p.Y + 3, Color.Black);
+            b.SetPixel(p.X + 4, p.Y + 4, Color.Black);
+            b.SetPixel(p.X + 5, p.Y + 5, Color.Black);
+            b.SetPixel(p.X + 6, p.Y + 6, Color.Black);
+            //mMouseMove("Form1", p.X, p.Y);
+            pictureBox1.Image = b;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        private const int WM_MOUSEMOVE = 0x0200;
+
+        public int MakeLParam(int LoWord, int HiWord)
+        {
+            return (int)((HiWord << 16) | (LoWord & 0xFFFF));
+        }
+
+        public void mMouseMove(string name, int y, int x)
+        {
+            IntPtr pl = FindWindowByCaption(IntPtr.Zero, name);
+
+            PostMessage(pl, WM_MOUSEMOVE, 1, MakeLParam(x, y));
+        }
+    }
+
+    class findPicture
+    {
+        private Image searchIn;
+        private List<List<Color>> list_searchIn;
+        private List<List<Color>> list_searchedFor;
+        private List<Point> maybe_points = new List<Point>();
+        private List<int> hits_list = new List<int>();
+
+        public findPicture(Image searchIn)
+        {
+            this.searchIn = searchIn;
+            list_searchIn = imageToList(searchIn);
+        }
+
+        public Point findPictureCoords(Image searchedFor)
+        {
+            list_searchedFor = imageToList(searchedFor);
+            int allPixels = searchedFor.Width * searchedFor.Height;
+
+            if (searchIn.Size.Width > searchedFor.Width && searchIn.Height > searchedFor.Size.Height)
+            {
+                for (int line = 0; line < searchIn.Height - searchedFor.Height; line++)
+                {
+                    for (int col = 0; col < searchIn.Width - searchedFor.Width; col++)
+                    {
+                        if (list_searchIn[col][line] == list_searchedFor[0][0] && list_searchIn[col + searchedFor.Width - 1][line] == list_searchedFor[searchedFor.Width - 1][0])
+                        {
+                            maybe_points.Add(new Point(col, line));
+                            int hits = 0;
+                            for (int y = 0; y < searchedFor.Height; y++)
+                            {
+                                for (int x = 0; x < searchedFor.Width; x++)
+                                {
+                                    if (list_searchIn[x + col][y + line] == list_searchedFor[x][y])
+                                    {
+                                        hits++;
+                                    }
+                                }
+                            }
+                            hits_list.Add(hits);
+                        }
+                    }
+                }
+            }
+
+            int highest = 0;
+            int i = 0;
+            while (i < hits_list.Count - 1)
+            {
+                if (hits_list[i] > highest)
+                {
+                    highest = i;
+                }
+                i++;
+            }
+
+            if (maybe_points[0] != null)
+            {
+                return maybe_points[highest];
+            }
+            else
+            {
+                return new Point(-1, -1);
+            }
+        }
+
+        public List<List<Color>> imageToList(Image img)
+        {
+            List<List<Color>> pixelList = new List<List<Color>>();
+            Bitmap bmp_pixels = new Bitmap(img);
+            for (int col = 0; col < img.Width; col++)
+            {
+                List<Color> lines = new List<Color>();
+                for (int line = 0; line < img.Height; line++)
+                {
+                    lines.Add(bmp_pixels.GetPixel(col, line));
+                }
+                pixelList.Add(lines);
+            }
+            return pixelList;
+        }
+    }
+
+}
